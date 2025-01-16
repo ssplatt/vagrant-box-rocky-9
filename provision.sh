7ae2083b31992e6b772b62ba5315cc7b6f46749e
Brett Taylor <brett.w.taylor@gmail.com>
2025-01-16 13:56:52 -0500

    add dnf automatic updates, nfs cifs tools (#2)

--- provision.sh
+++ provision.sh
@@ -52,7 +52,13 @@ sudo dnf install -y \
     openssl-devel \
     sqlite-devel \
     xz-devel \
-    zlib-devel
+    zlib-devel \
+    nfs-utils \
+    cifs-utils\
+    dnf-automatic
+
+sudo sed -i 's/apply_updates = no/apply_updates = yes/' /etc/dnf/automatic.conf
+sudo systemctl enable --now dnf-automatic.timer
 
 if [[ ! -s /etc/sudoers.d/vagrant ]]; then
     sudo bash -c "cat > /etc/sudoers.d/vagrant" <<EOL
